d023a5f10934547a4d7badf9feaaa7e9e3b6efe7
dinesh <dheenu02@gmail.com>
2023-10-15 13:56:54 +0530

    Assignment1

--- src/main/java/PageObject/Search.java
+++ src/main/java/PageObject/Search.java
@@ -1,2 +1,23 @@
-package PageObject;public class Search {
+package PageObject;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class Search {
+    WebDriver ldriver;
+    private static final By PRODUCTNAME = By.xpath("//input[@placeholder='Search For Products']");
+    private static final By SEARCH = By.xpath("//button[normalize-space()='Search']");
+
+    public Search(WebDriver rdriver) {
+        ldriver = rdriver;
+    }
+
+
+    public void fillProductname(String productname){
+        ldriver.findElement(PRODUCTNAME).sendKeys(productname);
+    }
+    public void fillSearch(){
+        ldriver.findElement(SEARCH).click();
+    }
+
 }
--- src/test/java/Tests/SearchTest.java
+++ src/test/java/Tests/SearchTest.java
@@ -1,14 +1,28 @@
+package Tests;
+
+import PageObject.Search;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 import java.time.Duration;
 
 public class SearchTest {
-    public static void main(String[] args) {
+    @Test
+    public void searchTesting1() {
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
         driver.get("https://ecommerce-playground.lambdatest.io/");
 
+        Search search = new Search(driver);
+        search.fillProductname("iphone");
+        search.fillSearch();
 
+//        Search search = new Search(driver);
+//        search.fillProductname("Tablets");
+//        search.fillSearch();
+
+        Assert.assertTrue(driver.getCurrentUrl().contains("Tablets"),"URL MISSMATCH");
     }
 }
