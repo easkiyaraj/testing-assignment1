03ecf968920b03c45fccd6162f297ff4f621a1c5
dinesh <dheenu02@gmail.com>
2023-12-01 17:51:16 +0530

    Assignment1

---
+++ src/main/java/PageObject/Myntra.java
@@ -0,0 +1,21 @@
+package PageObject;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class Myntra {
+    WebDriver ldriver;
+    private static final By MEN = By.xpath("//a[@class='desktop-main'][normalize-space()='Men']");
+    private static final By MENCOLLECTION = By.xpath("//div[@id='mountRoot']//div[2]//div[1]//div[1]//div[1]//div[1]//div[1]//div[1]//div[1]//div[1]//div[1]//div[1]//a[1]//div[1]//picture[1]//img[1]");
+
+    public Myntra(WebDriver rdriver) {
+        ldriver = rdriver;
+    }
+
+    public void fillMen(){
+        ldriver.findElement(MEN).click();
+    }
+    public void fillMencollection(){
+        ldriver.findElement(MENCOLLECTION).click();
+    }
+}
---
+++ src/test/java/Tests/MyntraTest.java
@@ -0,0 +1,19 @@
+package Tests;
+
+import PageObject.Myntra;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class MyntraTest {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        driver.get("https://www.myntra.com/");
+
+        Myntra myntra = new Myntra(driver);
+        myntra.fillMen();
+        myntra.fillMencollection();
+    }
+}
